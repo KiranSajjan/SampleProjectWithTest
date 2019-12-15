e4fd1adfdfe1a4be69276126e04545d7b2f7c361
KiranSajjan <45354989+KiranSajjan@users.noreply.github.com>
2019-12-15 13:57:07 +0530

    update

--- SampleProjectWithTest/Controllers/HomeController.cs
+++ SampleProjectWithTest/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,29 @@ namespace SampleProjectWithTest.Controllers
         {
             return View();
         }
+        public void dummyClass()
+        {
+            Account account = new Account
+            {
+                Email = "james@example.com",
+                Active = true,
+                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+                Roles = new List<string>
+                            {
+                                "User",
+                                "Admin"
+                            }
+            };
 
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+        }
+
+    }
+    public class Account
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IList<string> Roles { get; set; }
     }
 }
